5f193843112001504e54bda2c668ae021e48e09d
Bheem yadav <sonuydv1249@gmail.com>
2025-09-30 22:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
+app.use(cors({ origin: '*', credentials: true })); 
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello worldsss'));
