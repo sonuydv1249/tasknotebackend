a8efbbc5630f273220a01b013b161382c9e156b2
Bheem yadav <sonuydv1249@gmail.com>
2025-09-30 22:34:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://newtaskusername:bheem1249@cluster0.9v5rprm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
